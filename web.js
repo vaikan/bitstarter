49a6d00c2e2511a825b8272d9ee69c2fe0542cb6
Saurav Pokhrel <sauravpokhrel.sp@gmail.com>
2013-07-10 20:53:23 +0545

    first try

--- web.js
+++ web.js
@@ -1,9 +1,12 @@
+var fs = require('fs');
 var express = require('express');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  //response.send('Hello World 2!');
+  var content = readFileSync('index.html', 'utf8');
+  console.log(content);
 });
 
 var port = process.env.PORT || 5000;
