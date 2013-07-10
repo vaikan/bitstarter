77eb083376ebe32b7751db141d8242573a96f793
Saurav Pokhrel <sauravpokhrel.sp@gmail.com>
2013-07-10 20:59:43 +0545

    3rd try

--- web.js
+++ web.js
@@ -1,4 +1,5 @@
 var fs = require('fs');
+var buffer = new Buffer(16);
 var express = require('express');
 
 var app = express.createServer(express.logger());
@@ -6,7 +7,8 @@ var app = express.createServer(express.logger());
 app.get('/', function(request, response) {
   //response.send('Hello World 2!');
   var content = fs.readFileSync('index.html', 'utf8');
-  console.log(content);
+  //console.log(content);
+  buffer.toString(content, 'utf-8');
 });
 
 var port = process.env.PORT || 5000;
