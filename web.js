06cd9b0db687315b7810bfe81421b9ec8de66694
Saurav Pokhrel <sauravpokhrel.sp@gmail.com>
2013-07-10 21:09:27 +0545

    5th try

--- web.js
+++ web.js
@@ -5,8 +5,8 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  var content = fs.readFileSync('index.html', 'utf8');
-  response.send(content);
+  response.send(fs.readFileSync('index.html').toString());
+  
 
 });
 
